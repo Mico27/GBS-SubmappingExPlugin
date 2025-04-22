547e7f4339892eeb64f8dcb2fcdf2fa00a0fa50e
Mico27 <mickaelfortier616@msn.com>
2025-04-22 16:33:52 -0400

    have the plugin in the example project up to date

--- SubmapExamples/plugins/SubmappingExPlugin/engine/src/copy_scene_parts.c
+++ SubmapExamples/plugins/SubmappingExPlugin/engine/src/copy_scene_parts.c
@@ -175,6 +175,8 @@ void copy_background_submap_to_tileset(SCRIPT_CTX * THIS) OLDCALL BANKED {
 	background_t bkg;
     MemcpyBanked(&bkg, scn.background.ptr, sizeof(bkg), scn.background.bank);
     const tileset_t* tileset = bkg.tileset.ptr;
+	UWORD n_tiles = ReadBankedUWORD(&(tileset->n_tiles), bkg.tileset.bank);
+	UBYTE ui_reserved_offset = (n_tiles > 128 && n_tiles < 192)? (192 - n_tiles): 0;
 	unsigned char* tilemap_ptr = bkg.tilemap.ptr;
 	unsigned char* tilemap_attr_ptr = bkg.cgb_tilemap_attr.ptr;	
 	
@@ -182,17 +184,20 @@ void copy_background_submap_to_tileset(SCRIPT_CTX * THIS) OLDCALL BANKED {
 	
 	UBYTE buffer_size = sizeof(UBYTE) * width;
 	for (uint8_t i = 0; i < height; i++){
-		uint16_t source_offset = ((source_y + i) * (int16_t)bkg.width) + source_x;
-		uint16_t dest_offset = ((dest_y + i) * image_tile_width) + dest_x;
+		uint16_t source_offset = ((source_y + i) * (uint16_t)bkg.width) + source_x;
+		uint16_t dest_offset = ((dest_y + i) * (uint16_t)image_tile_width) + dest_x;
 		for (uint8_t j = 0; j < width; j++){	
-			UBYTE dest_tile = ReadBankedUBYTE(image_ptr + (dest_offset + j), image_bank);	
-			UBYTE source_tile = ReadBankedUBYTE(tilemap_ptr + (source_offset + j), bkg.tilemap.bank);				
+			UBYTE dest_tile = ReadBankedUBYTE(image_ptr + (uint16_t)(dest_offset + j), image_bank);	
+			UBYTE source_tile = ReadBankedUBYTE(tilemap_ptr + (uint16_t)(source_offset + j), bkg.tilemap.bank);
+			if (ui_reserved_offset && source_tile >= 128){
+				source_tile = source_tile - ui_reserved_offset;
+			}			
 			#ifdef CGB
 				if (_is_CGB) {			
 					
-					UBYTE dest_attr = ReadBankedUBYTE(image_attr_ptr + (dest_offset + j), image_attr_bank);	
-					if (copy_attributes){
-						UBYTE source_attr = ReadBankedUBYTE(tilemap_attr_ptr + (source_offset + j), bkg.cgb_tilemap_attr.bank);	
+					UBYTE dest_attr = ReadBankedUBYTE(image_attr_ptr + (uint16_t)(dest_offset + j), image_attr_bank);	
+					UBYTE source_attr = ReadBankedUBYTE(tilemap_attr_ptr + (uint16_t)(source_offset + j), bkg.cgb_tilemap_attr.bank);
+					if (copy_attributes){						
 						VBK_REG = 1; 
 						if (copy_attributes == 1){
 							set_bkg_tile_xy((dest_x + j) & 31, (dest_y + i) & 31, (dest_attr & 0x08)? (source_attr | 0x08): (source_attr & ~0x08));	
@@ -204,16 +209,26 @@ void copy_background_submap_to_tileset(SCRIPT_CTX * THIS) OLDCALL BANKED {
 					if (dest_attr & 0x08){ 					
 						VBK_REG = 1; 
 					}	
-				}
-			#endif
-			if (cgb_tileset){
-				SetBankedBkgData(dest_tile, 1, cgb_tileset->tiles + (source_tile << 4), bkg.cgb_tileset.bank);
-			} else {
-				SetBankedBkgData(dest_tile, 1, tileset->tiles + (source_tile << 4), bkg.tileset.bank);
-			}
-			#ifdef CGB
-				VBK_REG = 0;
+					if (cgb_tileset && (source_attr & 0x08)){
+						SetBankedBkgData(dest_tile, 1, cgb_tileset->tiles + (uint16_t)(source_tile << 4), bkg.cgb_tileset.bank);
+					} else {
+						SetBankedBkgData(dest_tile, 1, tileset->tiles + (uint16_t)(source_tile << 4), bkg.tileset.bank);
+					}
+					VBK_REG = 0;
+				} else {
+					SetBankedBkgData(dest_tile, 1, tileset->tiles + (uint16_t)(source_tile << 4), bkg.tileset.bank);
+				}					
+			#else				
+				SetBankedBkgData(dest_tile, 1, tileset->tiles + (uint16_t)(source_tile << 4), bkg.tileset.bank);				
 			#endif
 		}
 	}	
+}
+
+void vm_replace_background_tile(SCRIPT_CTX * THIS) OLDCALL BANKED {
+	set_bkg_tile_xy(((*(uint8_t *) VM_REF_TO_PTR(FN_ARG0)) & 31), ((*(uint8_t *) VM_REF_TO_PTR(FN_ARG1)) & 31), (*(uint8_t *) VM_REF_TO_PTR(FN_ARG2)));
+}
+
+void vm_replace_overlay_tile(SCRIPT_CTX * THIS) OLDCALL BANKED {
+	set_win_tile_xy(((*(uint8_t *) VM_REF_TO_PTR(FN_ARG0)) & 31), ((*(uint8_t *) VM_REF_TO_PTR(FN_ARG1)) & 31), (*(uint8_t *) VM_REF_TO_PTR(FN_ARG2)));		
 }
